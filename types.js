362cdcc7577485551a63d710f5a3a0f0271979e8
nrser <neil@ztkae.com>
2016-09-17 19:41:02 +0800

    change ComponentType -> ComponentClass type

--- types.js
+++ types.js
@@ -3,7 +3,7 @@ import type { $Refinement } from 'tcomb';
 import * as nrser from 'nrser';
 import { Component } from 'react';
 
-export const ComponentType = nrser.t.subclassOf(Component);
+export const ComponentClass = nrser.t.subclassOf(Component);
 
 function isId(string) {
   return string.length === 17;
