a4ca1a2e576ef15bde3e42dc602280ecfbba6815
nrser <neil@ztkae.com>
2016-09-18 07:38:45 +0800

    generalizing isId a bit

--- types.js
+++ types.js
@@ -1,12 +1,15 @@
 import tcomb from 'tcomb';
 import type { $Refinement } from 'tcomb';
 import * as nrser from 'nrser';
-import { Component } from 'react';
+import { Component, PropTypes as ReactPropTypes } from 'react';
 
 export const ComponentClass = nrser.t.subclassOf(Component);
 
-function isId(string) {
-  return string.length === 17;
+export const ID_LENGTH = 17;
+
+function isId(value) {
+  return (typeof value === 'string') && (value.length === ID_LENGTH);
 }
 
+// doesn't fucking work and defaults to 't.All'
 export type Id = string & $Refinement<typeof isId>;
